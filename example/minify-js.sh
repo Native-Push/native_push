972c05c5f0aa9de9de9895a4891ef3a575519ea0
Sven Op de Hipt <eisberg-zirkel-0c@icloud.com>
2024-05-16 14:16:32 +0200

    optimize scripts

--- example/minify-js.sh
+++ example/minify-js.sh
@@ -1,8 +1,6 @@
-#!/bin/bash
-
 cat web/native_push_sw_part.js web/native_push_sw_common.js > web/native_push_sw.js
 cat web/native_push_sw_non_localize_part.js web/native_push_sw_common.js > web/native_push_sw_non_localize.js
 
-uglifyjs web/native_push.js --compress -m toplevel=true -m "reserved=[native_push_initialNotification, native_push_initializeRemoteNotification, native_push_registerForRemoteNotification]" -o web/native_push.min.js
-uglifyjs web/native_push_sw.js --compress -m toplevel=true -o web/native_push_sw.min.js
-uglifyjs web/native_push_sw_non_localize.js --compress -m toplevel=true -o web/native_push_sw_non_localize.min.js
+uglifyjs web/native_push.js -c -m toplevel=true -m "reserved=[native_push_initialNotification, native_push_initializeRemoteNotification, native_push_registerForRemoteNotification]" -o web/native_push.min.js
+uglifyjs web/native_push_sw.js -c -m toplevel=true -o web/native_push_sw.min.js
+uglifyjs web/native_push_sw_non_localize.js -c -m toplevel=true -o web/native_push_sw_non_localize.min.js
--- extract_fcm_options.sh
+++ extract_fcm_options.sh
@@ -1,15 +1,1 @@
-#!/bin/bash
-
-JSON=$(cat google-services.json);
-PROJECT_ID=$(echo "$JSON" | jq -r ".project_info.project_id");
-CLIENTS=$(echo "$JSON" | jq -c -r ".client.[] | {packageName: .client_info.android_client_info.package_name, appId: .client_info.mobilesdk_app_id}");
-
-for CLIENT in $CLIENTS; do
-  if [ "$(echo "$CLIENT" | jq -r ".packageName")" = "$1" ]; then
-    echo "$CLIENT" | jq -r "{projectId: \"$PROJECT_ID\", applicationId: .appId}";
-    exit 0;
-  fi;
-done;
-
-echo "App with package name $1 not found.";
-exit 1;
+cat - | jq "{projectId: .project_info.project_id, applicationId: .client.[] | select(.client_info.android_client_info.package_name==\"$1\") | .client_info.mobilesdk_app_id}";
